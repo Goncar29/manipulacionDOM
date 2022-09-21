39fde0ecc7492377c53814e95aada1448147d0b6
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2022-09-21 12:43:45 -0300

    Agregamos algunos estilos

--- src/index.js
+++ src/index.js
@@ -23,21 +23,31 @@ window
 
             // crear imagen
             const imagen = document.createElement('img');
+            imagen.className = "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6";
             // URL de la imagen
             imagen.src = `${baseurl}${item.image}`
 
             // crear titulo
             const titulo = document.createElement('h2');
+            titulo.className = "text-lg";
             titulo.textContent = item.name
 
             // crear precio
-            const precio = document.createElement('div');
+            const precio = document.createElement('div');            titulo.className = "text-lg";
+            precio.className = "text-gray-600";
             precio.textContent = item.price;
-            // creamos su contenedor
-            const container = document.createElement('div');
-            container.append(imagen, titulo, precio);
+            
+            // wrap precio y titulo
+            const precioYTitulo = document.createElement('div');
+            precioYTitulo.className = "text-center md:text-left";
+            precioYTitulo.append(titulo, precio);
 
-            todosLosItems.push(container);
+            // wrap imagen y precioYTitulo
+            const card = document.createElement('div');
+            card.className = "md:flex bg-white rounded-lg p-6 hover:bg-gray-300";
+            card.append(imagen, precioYTitulo);
+
+            todosLosItems.push(card);
         });
         appNode.append(...todosLosItems);
     });
